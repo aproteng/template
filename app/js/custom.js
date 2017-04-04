3ccfd35eb29b830f4e34c6b91c6ccf9997e1cc5d
Yastopchik <yastopch@gmail.com>
2017-04-04 23:33:18 +0300

    add bx slider

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,12 +1,21 @@
 $(document)
-    .ready(function() {
-		$('ul.sf-menu').superfish({
-                  delay: 1000,
-                  animation: {
-                      opacity: 'show',
-                      height: 'show'
-                  },
-                  speed: 'normal',
-                  autoArrows: false                  
-              });
-});
+    .ready(function () {
+        $('ul.sf-menu').superfish({
+            delay: 1000,
+            animation: {
+                opacity: 'show',
+                height: 'show'
+            },
+            speed: 'normal',
+            autoArrows: false
+        })
+        $('.scrollpartners').bxSlider({
+            minSlides: 4,
+            maxSlides: 8,
+            slideWidth: 170,
+            slideMargin: 10,
+            ticker: true,
+            speed: 25000,
+            autoControls: true, 
+        });
+    });
